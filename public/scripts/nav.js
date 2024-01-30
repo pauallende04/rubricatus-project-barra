fc7098fef2eae385ba3f2cdba65e93944bb6f890
pauallende <pauallendeherraiz@gmail.com>
2024-01-30 17:23:54 +0100

    nav mod

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,3 +1,26 @@
 console.log('nav.js loaded');
 
-function ca
+async function cambiarNav(){
+    var nav = document.getElementById("nav");
+    if(nav.classList.contains("navbar-light")){
+        await sleep(100);
+        nav.classList.remove("navbar-light");
+        nav.classList.add("navbar-dark", "bg-dark");
+        document.body.style.backgroundColor = "#f8f9fa";
+    }else{
+        nav.classList.add("navbar-light");
+        nav.classList.remove("navbar-dark", "bg-dark");
+        document.body.style.backgroundColor = "transparent";
+    }
+}
+function sleep(ms) {return new Promise((res)=> setTimeout(res, ms));}
+
+window.addEventListener('load', ()=>{cambiarNav()});
+
+$('#modoOscuro').click(cambiarNav);
+$(document).ready(function(){
+    $('.nav-link').on('click', function(){
+        $('.nav-item').removeClass('active');
+        $(this).closest('.nav-item').addClass('active');
+    });
+});
